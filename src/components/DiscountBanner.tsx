a795dfa5524816bbf8af41568c782912e14cd24f
AbubakarMd <Mdabu.jsr.121@gmail.com>
2025-09-22 23:57:28 +0530

    offer

--- src/components/DiscountBanner.tsx
+++ src/components/DiscountBanner.tsx
@@ -1,10 +1,11 @@
+import banner from "@/assets/discount offer.png";
 const DiscountBanner = () => {
   return (
     <section className="py-5 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <img 
-            src="https://i.ibb.co/nsHZ5zNY/Untitled-2.png" 
+            src={banner} 
             alt="Special Discount Banner"
             className="w-full max-w-4xl mx-auto rounded-lg shadow-glow"
           />
